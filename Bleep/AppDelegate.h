800913a8fdb52cd6fc160e069764b5a1e8860257
Cody <cody.ko@gmail.com>
2015-04-16 15:11:04 -0700

    Initial Commit

---
+++ Bleep/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Bleep
+//
+//  Created by Cody Kolodziejzyk on 4/16/15.
+//  Copyright (c) 2015 sick.af. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Bleep/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Bleep
+//
+//  Created by Cody Kolodziejzyk on 4/16/15.
+//  Copyright (c) 2015 sick.af. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
